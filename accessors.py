4e6614f6825f006e2136b3c39da1354be19aa8b6
dcherian <dcherian@mit.edu>
2018-11-20 10:41:31 -0700

    Update MonsoonAccessor: Add splitlabels
    1. Remove .ne, .sw etc. since I don't use them.

--- accessors.py
+++ accessors.py
@@ -13,37 +13,33 @@ class MonsoonAccessor(object):
                          'SW': [5, 6, 7, 8, 9],
                          'SWNE': [10, 11]}
 
+        self._splitseasons = {'NE': [12, 1, 2],
+                              'Mar': [3],
+                              'Apr': [4],
+                              'SW': [5, 6, 7, 8, 9],
+                              'SWNE': [10, 11]}
+
         self._labels = np.asarray(['    '] * len(xarray_obj))
+        self._splitlabels = np.asarray(['    '] * len(xarray_obj))
 
         for ss in self._seasons:
             self._labels[self._obj.time.dt.month.isin(
                 self._seasons[ss]).values] = ss
 
-    def _monsoon_func(self, label):
-        obj_type = type(self._obj)
-
-        return obj_type(self._obj.where(self._labels == label.upper()),
-                        name=self._obj.name + ' | ' + label.upper(),
-                        coords=self._obj.coords, dims=self._obj.dims)
-
-    @property
-    def ne(self):
-        return self._monsoon_func('ne')
-
-    @property
-    def nesw(self):
-        return self._monsoon_func('nesw')
-
-    @property
-    def sw(self):
-        return self._monsoon_func('sw')
-
-    @property
-    def swne(self):
-        return self._monsoon_func('swne')
+        for ss in self._splitseasons:
+            self._splitlabels[self._obj.time.dt.month.isin(
+                self._splitseasons[ss]).values] = ss
 
     @property
     def labels(self):
         obj_type = type(self._obj)
         return obj_type(self._labels, name='monsoon', coords=self._obj.coords,
+                        dims=self._obj.dims)
+
+    @property
+    def splitlabels(self):
+        obj_type = type(self._obj)
+        return obj_type(self._splitlabels,
+                        name='monsoon',
+                        coords=self._obj.coords,
                         dims=self._obj.dims)
